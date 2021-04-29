ff5cbebf0797da7088f7103879a765ccba10eeb2
kd Bromley <kd.bromley1@gmail.com>
2021-04-29 16:05:15 -0700

    installed react-router, created routes

--- src/App.js
+++ src/App.js
@@ -1,22 +1,43 @@
+import { Component } from 'react';
 import LandingPage from './LandingPage/LandingPage';
-import './App.css';
 import Login from './Login/Login';
 import Register from './Register/Register';
+import './App.css';
+import { Route } from 'react-router-dom';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <h1>on AUX</h1>
-      </header>
-      <main>
-        {/* routes */}
-        <LandingPage />
-        <Login />
-        <Register />
-      </main>
-    </div>
-  );
+class App extends Component {
+  renderRoutes() {
+    return (
+     <>
+      <Route 
+       exact
+       path={['/', '/login', '/register']}
+       component={LandingPage}
+      />
+      <Route
+       path='/login'
+       component={Login}
+      />
+      <Route
+       path='/register'
+       component={Register}
+      />
+     </>
+    )
+  }
+  
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <h1>on AUX</h1>
+        </header>
+        <main>
+          {this.renderRoutes()}
+        </main>
+      </div>
+    );
+  }
 }
 
 export default App;
