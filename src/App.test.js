0184ae686319c014b9e2378662a7142898109f1b
kd Bromley <kd.bromley1@gmail.com>
2021-05-02 10:30:06 -0700

    added smoke tests and updated scripts

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,20 @@
-import { render, screen } from '@testing-library/react';
+//import { render, screen } from '@testing-library/react';
+import ReactDOM from 'react-dom';
+import { BrowserRouter } from 'react-router-dom';
 import App from './App';
 
-test('renders learn react link', () => {
+/* test('renders learn react link', () => {
   render(<App />);
   const linkElement = screen.getByText(/learn react/i);
   expect(linkElement).toBeInTheDocument();
-});
+}); */
+
+it('renders without crashing', () => {
+  const div = document.createElement('div');
+  ReactDOM.render(
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>,
+  div);
+  ReactDOM.unmountComponentAtNode(div);
+})
